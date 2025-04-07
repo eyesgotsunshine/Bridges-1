85372e96dbdb20427de3860fa04eae71f2d26860
siadto_0923258 <303faerie@gmail.com>
2025-04-07 13:38:04 -0700

    did void moveStack

--- toh.cpp
+++ toh.cpp
@@ -10,10 +10,49 @@ using namespace bridges;
 
 void moveStack(Hanoi& nt, int from, int to, int howmany) {
   //TODO
-  
+   if (howmany == 1) {
+        nt.move(from, to);
+    }
+    else {
+        int other = 6 - (from + to);
+        moveStack(nt, from, other, howmany - 1);
+        nt.move(from, to);
+        moveStack(nt, other, to, howmany -1);
+    }
 }
 
-// 
+// 31 steps for 5 disks, formula is 2^n - 1
+//  1 move(1, 2);
+//  2 move(1, 3);
+//  3 move(2, 3);
+//  4 move(1, 2);
+//  5 move(3, 1);
+//  6 move(3, 2);
+//  7 move(1, 2);
+//  8 move(1, 3);
+//  9 move(2, 3);
+//  10 move(2, 1);
+//  11 move(3, 1);
+//  12 move(2, 3);
+//  13 move(1, 2);
+//  14 move(1, 3);
+//  15 move(2, 3);
+//  16 move(1, 2);
+//  17 move(3, 1);
+//  18 move(3, 2);
+//  19 move(1, 2);
+//  20 move(3, 1);
+//  21 move(2, 3);
+//  22 move(2, 1);
+//  23 move(3, 1);
+//  24 move(3, 2);
+//  25 move(1, 2);
+//  26 move(1, 3);
+//  27 move(2, 3);
+//  28 move(1, 2);
+//  29 move(3, 1);
+//  30 move(3, 2);
+//  31 move(1, 2);  
 
 int main() {
   //create Bridges object
